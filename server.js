208b41d297e5f8aa5fdc8e8c656a163e12e54748
shub1905 <shub1905@gmail.com>
2016-03-05 18:33:57 -0500

    server & package files

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+
+const PORT = 8080;
+
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
