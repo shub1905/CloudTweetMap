7ad35caa5a09cea7dd9a1e946dd759ccb70851a2
Aishwarya Iyer <ai2336@columbia.edu>
2016-04-13 19:10:52 -0400

    store tweets in sqs

--- twitter.js
+++ twitter.js
@@ -1,11 +1,43 @@
 var Twit = require('twit');
 var fs = require('fs');
 var http = require('http');
-var es = require('./elasticsearch_model.js');
+//var es = require('./elasticsearch_model.js');
+var aws = require('aws-sdk');
+var queueUrl = ""
 
 var config_raw = fs.readFileSync('config.json');
 var config = JSON.parse(config_raw).twitter;
 
+aws.config.loadFromPath(./aws_config.json);
+
+var sqs = aws.SQS();
+var params = {
+  QueueName: "tweets"
+};
+
+sqs.createQueue(params, function(err, data) {
+   if (err)
+	console.log(err, err.stack); // an error occurred
+   else     
+	console.log(data);           // successful response
+ /*  
+   data = {
+    QueueUrl: "https://queue.amazonaws.com/012345678910/MyQueue"
+   }
+*/   
+ });
+
+var params = {
+  QueueName: 'tweets'
+};
+sqs.getQueueUrl(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else    {
+ queueUrl = data;	
+ console.log(data);           // successful response
+}
+
+});
 
 var T = new Twit({
     consumer_key: config.consumer_key,
@@ -15,8 +47,19 @@ var T = new Twit({
     timeout_ms: config.timeout
 })
 
-var stream = T.stream('statuses/filter', { track: '#VoteTrump,#MakeAmericaGreatAgain,#Trump2016,#MakeDonaldDrumpfAgain,#DonaldDrumpf,#FeelTheBurn,#Bernie2016,#Hilary2016,#IAmWithHer,#Elections2016,#GOPDebate,#repulican,#democrat' });
+var stream = T.stream('statuses/filter', { track: '#VoteTrump,#MakeAmericaGreatAgain,#MakeAmericaGrateAgain,#Trump2016,#MakeDonaldDrumpfAgain,#DonaldDrumpf,#FeelTheBern,#Bernie2016,#Hilary2016,#IAmWithHer,#Elections2016,#GOPDebate,#repulican,#democrat,#HilarySoQualified,trump,bernie,hilary,president,elections,nuclear' });
 
 stream.on('tweet', function(tweet, error) {
-    es.index(tweet);
+    if(tweet.geo!=null) {
+    var params = {
+    MessageBody: JSON.stringify(tweet),
+    QueueUrl: queueUrl
+  };
+  sqs.sendMessage(params, function(err, data) {
+    if (err) common.logError(err, err.stack); // an error occurred
+    else     common.log(data);           // successful response
+  });  
+}
+    //es.index(tweet);
+}
 });
